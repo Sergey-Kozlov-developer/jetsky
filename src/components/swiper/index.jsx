4e75f94789744ba72a7eec8c37c3ecdd09eb79b7
Sergey-Kozlov-developer <vmf.serge.kozlov@gmail.com>
2024-05-14 17:43:38 +0300

    update ui swiper block

--- src/components/swiper/index.jsx
+++ src/components/swiper/index.jsx
@@ -16,53 +16,70 @@ import homeImg2 from "../../assets/img/home/homeImg2.jpg";
 import homeImg3 from "../../assets/img/home/homeImg3.webp";
 import homeImg4 from "../../assets/img/home/homeImg4.jpg";
 
+import motoImg from "../../assets/img/home/moto.png";
+
 export default function App() {
     return (
         <>
-            <div className={styles.swiper__container}>
-                <Swiper
-                    spaceBetween={30}
-                    centeredSlides={true}
-                    autoplay={{
-                        delay: 2500,
-                        disableOnInteraction: false,
-                    }}
-                    pagination={{
-                        clickable: true,
-                    }}
-                    navigation={true}
-                    modules={[Autoplay, Pagination, Navigation]}
-                    className="mySwiper"
-                >
-                    <SwiperSlide>
-                        <img
-                            className={styles.swiper__img}
-                            src={homeImg1}
-                            alt=""
-                        />
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <img
-                            className={styles.swiper__img}
-                            src={homeImg2}
-                            alt=""
-                        />
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <img
-                            className={styles.swiper__img}
-                            src={homeImg3}
-                            alt=""
-                        />
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <img
-                            className={styles.swiper__img}
-                            src={homeImg4}
-                            alt=""
-                        />
-                    </SwiperSlide>
-                </Swiper>
+            <div className={styles.swiper__content}>
+                <div className={styles.swiper__container}>
+                    <Swiper
+                        spaceBetween={30}
+                        centeredSlides={true}
+                        autoplay={{
+                            delay: 2500,
+                            disableOnInteraction: false,
+                        }}
+                        pagination={{
+                            clickable: true,
+                        }}
+                        navigation={true}
+                        modules={[Autoplay, Pagination, Navigation]}
+                        className="mySwiper"
+                    >
+                        <SwiperSlide>
+                            <img
+                                className={styles.swiper__img}
+                                src={homeImg1}
+                                alt=""
+                            />
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <img
+                                className={styles.swiper__img}
+                                src={homeImg2}
+                                alt=""
+                            />
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <img
+                                className={styles.swiper__img}
+                                src={homeImg3}
+                                alt=""
+                            />
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <img
+                                className={styles.swiper__img}
+                                src={homeImg4}
+                                alt=""
+                            />
+                        </SwiperSlide>
+                    </Swiper>
+                </div>
+                <div className={styles.swiper__content_right}>
+                    <img src={motoImg} alt="" />
+                    <p>Лодочный мотор Suzuki DF9.9BRS</p>
+                    <div className={styles.sale_content}>
+                        <p className={styles.sale__text}>
+                            Акция действует до
+                            <br />
+                            <p className={styles.sale__text_color}>
+                                31.08.2020
+                            </p>
+                        </p>
+                    </div>
+                </div>
             </div>
         </>
     );
